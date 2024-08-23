9b1eb5b1d6d3ee9de055ce800c53f52d620bcd25
kiiroisenko786 <projectyuni5@gmail.com>
2024-08-24 00:59:01 +0100

    implement signup and save password basic ver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UsersRepository]),
-  ],
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UsersRepository],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "./user.entity";
 import { DataSource, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
   }
 }
