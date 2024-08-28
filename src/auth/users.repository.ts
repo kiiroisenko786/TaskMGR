9c862ba05fa9d4267348dc1ec918da662bd37b69
kiiroisenko786 <projectyuni5@gmail.com>
2024-08-28 01:15:42 +0100

    use bcrypt to hash and salt pword

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,7 +2,7 @@ import { ConflictException, Injectable, InternalServerErrorException } from "@ne
 import { User } from "./user.entity";
 import { DataSource, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
-import e from "express";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -12,7 +12,11 @@ export class UsersRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
