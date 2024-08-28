f5340e09c8cc31cb963e070ec9924026294936e1
kiiroisenko786 <projectyuni5@gmail.com>
2024-08-29 00:21:54 +0100

    implement signin logic£

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,14 @@ export class AuthController {
     constructor(
         private authService: AuthService,
     ) {}
+
     @Post('/signup')
     signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('/signin')
+    signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        return this.authService.signIn(authCredentialsDto);
     }
 }
