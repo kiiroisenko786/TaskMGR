d75a40316851554da35aa16a9b6ee6ae9fa22553
kiiroisenko786 <projectyuni5@gmail.com>
2024-08-30 01:46:04 +0100

    guard routes implementing authguard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,11 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './tasks.repository';
 import { Task } from './task.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule, PassportModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
